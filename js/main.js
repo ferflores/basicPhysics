74c2431be86374045e924d9c32a385c34251466c
root <root@localhost.localdomain>
2014-10-04 10:05:57 -0500

    separate slides in slides.js

--- js/main.js
+++ js/main.js
@@ -74,160 +74,16 @@ Main.prototype.erase = function(){
 Main.prototype.drawParticle = function(p, r){
 	var c = this.context;
 
-	var particle = p || this.particle;
-
 	c.beginPath();
 	c.fillStyle = "#000";
 	//c.arc(x, y, radious, sAngle,eAngle,clockwise);
-	c.arc(particle.x, particle.y, r || 20, 0, Math.PI*2, false);
+	c.arc(p.x, p.y, r || 20, 0, Math.PI*2, false);
 	c.closePath();
 	c.fill();
 }
 
-Main.prototype.commonCoordsConfig = function(){
-}
-
-Main.prototype.commonCoords = function(){
-	var context = this.context;
-
-	context.fillStyle = "#000000";
-	context.fillText(this.cx + "px,"+this.cy+"px", this.cx-50, this.cy+20);
-
-	context.beginPath();
-	context.lineWidth = 5;
-	context.strokeStyle = "#000000"
-	context.moveTo(this.cx, this.cy);
-	context.lineTo(this.cx,this.cy-250);
-	context.moveTo(this.cx, this.cy);
-	context.lineTo(this.cx + 250, this.cy);
-	context.closePath();
-	context.stroke();
-
-
-	context.fillText("x", this.cx+260, this.cy);
-	context.fillText("y", this.cx, this.cy-260);
-
-}
-
-Main.prototype.particleCenterConfig = function(){
-	this.particle = {
-		x:this.cx,
-		y:this.cy
-	}
-}
-
-Main.prototype.particleCenter = function(){
-	this.drawParticle();
-}
-
-Main.prototype.linearMovementConfig = function(){
-	this.particle = {
-		x:0,
-		y:this.cy
-	}
-}
-
-Main.prototype.linearMovement = function(){
-	this.drawParticle();
-
-	this.particle.x++;
-}
-
-Main.prototype.directionAngleConfig = function(){
-	var angle = 45 * Math.PI/180;
-	var speed = 5;
-
-	this.particle = {
-		x:this.cx,
-		y:this.cy,
-		vx: Math.cos(angle) * speed,
-		vy: Math.sin(angle) * speed
-	}
-}
-
-Main.prototype.directionAngle = function(){
-
-	this.drawParticle();
-
-	this.particle.x += this.particle.vx;
-	this.particle.y += this.particle.vy;
-
-}
-
-Main.prototype.accelerationConfig = function(){
-	var angle = 0;
-	var speed = 5;
-
-	this.particle = {
-		x:100,
-		y:this.cy,
-		vx: Math.cos(angle) * speed,
-		vy: 0,
-		acceleration: .5
-	}
-}
-
-Main.prototype.acceleration = function(){
-
-	this.drawParticle();
-
-	this.particle.x += this.particle.vx;
-
-	this.particle.vx += this.particle.acceleration;
-}
-
-Main.prototype.gravityConfig = function(){
-	var angle = 270;
-	var speed = 50;
-
-	this.particle = {
-		x:this.cx,
-		y:this.cy,
-		vx: 0,
-		vy: Math.sin(angle) * speed,
-		gravity: .2
-	}
-}
-
-Main.prototype.gravity = function(){
-
-	this.drawParticle();
-
-	this.particle.y += this.particle.vy;
-
-	this.particle.vy += this.particle.gravity;
-}
-
-Main.prototype.fireworksConfig = function(){
-
-	this['particles'] = [];
-
-	for (var i = 0; i < 100; i++) {
-		var randomAngle = Math.random()*Math.PI*2;
-		var randomSpeed = Math.random()*10;
-		this.particles.push({
-			x:this.cx,
-			y:this.cy,
-			vx: Math.cos(randomAngle)*randomSpeed,
-			vy: Math.sin(randomAngle)*randomSpeed,
-			gravity:.2
-		})
-	}
-}
-
-Main.prototype.fireworks = function(){
-
-	for (var i = 0; i < this.particles.length; i++) {
-
-		this.particles[i].x += this.particles[i].vx;
-		this.particles[i].y += this.particles[i].vy;
-		this.particles[i].vy += this.particles[i].gravity;
-		this.drawParticle(this.particles[i], 5);
-	};
-}
-
 window.onload = function(){
-	require(["canvasGrid"], function(required) {
+	require(["canvasGrid","slides"], function(required) {
 
 		var main = new Main();
 		main.configure();
